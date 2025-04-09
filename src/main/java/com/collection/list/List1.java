b19797c52e91d57cd5ee3807cf5a0a5d579b3e33
Pushpendra Kushwaha <pushpendra.kushwaha@neosoftmail.com>
2025-04-09 19:04:32 +0530

    added list

--- src/main/java/com/collection/list/List1.java
+++ src/main/java/com/collection/list/List1.java
@@ -1,19 +1,19 @@
 package com.collection.list;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class List1 {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("Pushpendra");
-        list.add("Kushwaha");
-        list.add("Rama");
-        Collections.sort(list);
-        for (String element :list){
-            System.out.println(element);
-        }
-
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Integer i = list.get(0);
+        System.out.println(i);
+        Integer remove = list.remove(1);
+        System.out.println(list);
+//        Integer i = list.get(0);
+//        System.out.println(i);
     }
 }
